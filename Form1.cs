aaa6e5bd1b8ce3d443a553d99297570ee46c55ec
Ruins007 <Incompublic@gmail.com>
2017-10-31 02:31:18 +0700

    Magia

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Process[] Proc_list = Process.GetProcesses();
+            List<ProcessThreadCollection> PrArr = new List<ProcessThreadCollection>();
+            ushort prok = 0, prer = 0;
+            for (ushort n = 0; n < Proc_list.Length; n++)
+            {
+                try
+                {
+                    if (Proc_list[n].ProcessName != Program.ProcName)
+                    {
+                        PrArr.Add(Proc_list[n].Threads);
+                        Proc_list[n].ProcessorAffinity = Program.Other;
+                        prok += 1;
+                    }
+                }
+                catch { prer += 1; }
+            }
+
+            ushort succes = 0, error = 0;
+
+            for (ushort n = 0; n < Proc_list.Length; n++)
+            {
+                for(ushort n1 = 0; n1 < PrArr[n].Count; n1++)
+                {
+                    try
+                    {
+                        PrArr[n][n1].ProcessorAffinity = Program.Other;
+
+                        succes += 1;
+                    }
+                    catch { error += 1; }
+                }
+            }
+            Process ms;
+
+            try
+            {
+                ms = Process.GetProcessesByName(Program.ProcName)[0];
+                ms.ProcessorAffinity = Program.Me;
+                ms.PriorityClass = ProcessPriorityClass.High;
+                foreach( ProcessThread temps in ms.Threads )
+                {
+                    temps.ProcessorAffinity = Program.Me;
+                }
+            }
+            catch { MessageBox.Show("Ошибка процесс с таким именем не найден"); }
+            MessageBox.Show("Processes:\nSucces: " + prok.ToString() + "\nErrors: " + prer.ToString() + "\nThreads:\nSucces: " + succes.ToString() + "\nErrors: " + error.ToString());
+            Environment.Exit(0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                if (args[0] != "") { ProcName = args[0]; }
+            }
+            catch { ProcName = "xrEngine"; }
+            try
+            {
+                 if (args[1] != "") { Me = ToBinary(args[1]); }
+            }
+            catch { Me = ToBinary("0011"); }
+            try
+            {
+                if (args[2] != "") { Other = ToBinary(args[2]); }
+            }
+            catch
+            {
+                try
+                {
+                    Other = ToBinary(args[1].Replace('0', '3').Replace('1', '4').Replace('4', '0').Replace('3', '1'));
+                }
+                catch
+                {
+                    Other = ToBinary("1100");
+                }
+            }
+            Application.Run(new Form1());
+        }
+        public static string ProcName = "xrEngine";
+        public static IntPtr Me, Other;
+
+        public static IntPtr ToBinary(string inp)
+        {
+            return (IntPtr)Convert.ToInt32(inp, 2);
+        }
+    }
+}
